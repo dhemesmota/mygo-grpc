fafb732c4e71111c400f3ce4cf8e4debf04f981a
Dhemes Mota <dhemes.mota@gmail.com>
2023-07-09 10:31:42 -0300

    feat: aplicacao simples em go, com gRPC

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/dhemesmota/mygo-grpc/internal/database"
+	"github.com/dhemesmota/mygo-grpc/internal/pb"
+	"github.com/dhemesmota/mygo-grpc/internal/service"
+	_ "github.com/mattn/go-sqlite3"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"net"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
